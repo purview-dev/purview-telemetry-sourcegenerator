706afa5940d6aa0e52869dac066e813a03bc193e
Kieron Lanning <kieron@outlook.com>
2024-03-04 19:53:09 +0000

    chore(structure): inital commit of project structure

---
+++ src/.build-system/msbuild/Properties/AssemblyInfo_Shared.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+
+[assembly: AssemblyCompany("Purview")]
+[assembly: AssemblyCopyright("Purview © 2024 | All rights reserved.")]
