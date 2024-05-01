b5c18c9242a265958fa25b5bd35dc33e44cb14fd
Kieron <kieronlanning@users.noreply.github.com>
2024-05-01 16:45:26 +0100

    docs: fixed invalid xml comments in attributes

--- src/Purview.Telemetry.SourceGenerator/Templates/Sources/Metrics/ObservableGaugeAttribute.cs
+++ src/Purview.Telemetry.SourceGenerator/Templates/Sources/Metrics/ObservableGaugeAttribute.cs
@@ -1,7 +1,7 @@
 ﻿namespace Purview.Telemetry.Metrics;
 
 /// <summary>
-/// Specifies the meter type generated corresponds to a <see cref="System.Diagnostics.Metrics.ObservableGuage{T}"/>.
+/// Specifies the meter type generated corresponds to a <see cref="System.Diagnostics.Metrics.ObservableGauge{T}"/>.
 /// </summary>
 [System.AttributeUsage(System.AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
 [System.Diagnostics.Conditional("PURVIEW_TELEMETRY_ATTRIBUTES")]
