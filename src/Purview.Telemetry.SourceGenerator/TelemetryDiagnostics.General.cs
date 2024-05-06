4e0239850937fa46698820724d00291ec7ce0b5b
Kieron <kieronlanning@users.noreply.github.com>
2024-05-06 09:52:34 +0100

    fix: fixed diagnostic id duplication

--- src/Purview.Telemetry.SourceGenerator/TelemetryDiagnostics.General.cs
+++ src/Purview.Telemetry.SourceGenerator/TelemetryDiagnostics.General.cs
@@ -17,7 +17,7 @@ partial class TelemetryDiagnostics
 
 		public static readonly TelemetryDiagnosticDescriptor InferenceNotSupportedWithMultiTargeting = new(
 			Id: "TSG1001",
-			Title: "Inferring generation targets is not supported when using multi-target generation.",
+			Title: "Inferring generation targets is not supported when using multi-target generation",
 			Description: $"When using multiple generation targets - Activities, Logs or Metrics, each method must be either excluded or have an explicit generation target: " +
 				$"{Constants.Activities.ActivityAttribute.Name}, {Constants.Activities.EventAttribute.Name}, {Constants.Activities.ContextAttribute.Name}, {Constants.Logging.LogAttribute.Name}, " +
 				$"{Constants.Metrics.CounterAttribute.Name}, {Constants.Metrics.HistogramAttribute.Name}, {Constants.Metrics.UpDownCounterAttribute.Name}, " +
@@ -28,8 +28,8 @@ partial class TelemetryDiagnostics
 
 		public static readonly TelemetryDiagnosticDescriptor MultiGenerationTargetsNotSupported = new(
 			Id: "TSG1002",
-			Title: "Only single generation types are supported.",
-			Description: $"Only a single generation target types (Activities, Logs or Metrics) is supported on methods. Use one of the following: " +
+			Title: "Multiple generation types are not supported",
+			Description: $"Only a single generation target types (Activities, Logs or Metrics) are supported. Use one of the following: " +
 				$"{Constants.Activities.ActivityAttribute.Name}, {Constants.Activities.EventAttribute.Name}, {Constants.Activities.ContextAttribute.Name}, {Constants.Logging.LogAttribute.Name}, " +
 				$"{Constants.Metrics.CounterAttribute.Name}, {Constants.Metrics.HistogramAttribute.Name}, {Constants.Metrics.UpDownCounterAttribute.Name}, " +
 				$"{Constants.Metrics.ObservableCounterAttribute.Name}, {Constants.Metrics.ObservableGaugeAttribute.Name} or {Constants.Metrics.ObservableUpDownCounterAttribute.Name}.",
