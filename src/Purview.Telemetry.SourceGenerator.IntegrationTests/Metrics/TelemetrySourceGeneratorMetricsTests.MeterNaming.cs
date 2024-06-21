68c81fafb04792d028f022d1264a99cbfdfeda7f
Kieron Lanning <kieronlanning@users.noreply.github.com>
2024-06-21 08:56:51 +0100

    test: added test to validate meter prefix and specific meter name

--- src/Purview.Telemetry.SourceGenerator.IntegrationTests/Metrics/TelemetrySourceGeneratorMetricsTests.MeterNaming.cs
+++ src/Purview.Telemetry.SourceGenerator.IntegrationTests/Metrics/TelemetrySourceGeneratorMetricsTests.MeterNaming.cs
@@ -79,6 +79,32 @@ interface ITestMetrics
 	}
 
 	[Fact]
+	public async Task Generate_GivenNameWithAssemblyAndInterfacePrefixAndName_GeneratesMetricsWithPrefix()
+	{
+		// Arrange
+		var basicMetric = @$"
+using Purview.Telemetry.Metrics;
+
+[assembly: MeterGeneration(InstrumentPrefix = ""This.Is.An.Assembly.Prefix"")]
+
+namespace Testing;
+
+[Meter(""testing-meter"", InstrumentPrefix = ""This.Is.A.Prefix"")]
+interface ITestMetrics 
+{{
+	[AutoCounter(""auto-counter"")]
+	void AutoCounterMetric();
+}}
+";
+
+		// Act
+		var generationResult = await GenerateAsync(basicMetric);
+
+		// Assert
+		await TestHelpers.Verify(generationResult);
+	}
+
+	[Fact]
 	public async Task Generate_GivenNameShouldBeLowerCase_GeneratesMetricsWithLowercaseName()
 	{
 		// Arrange
